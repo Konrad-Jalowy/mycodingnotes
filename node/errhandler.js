93da4be587fc7229bbe5d7018d058b2465fdc332
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-20 09:32:03 +0100

    errhandler

---
+++ node/errhandler.js
@@ -0,0 +1,7 @@
+exports.errorHandler = (err, req, res, next) => {
+    res.status(500).json({"Error": "Some kind of error occurred."});
+};
+
+exports.notFound = function(req, res){
+    res.status(404).render("404");
+  }
