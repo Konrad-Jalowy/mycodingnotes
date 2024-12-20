6324d6aa2d078659a8d51886f98ef7db641a4644
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-20 11:12:04 +0100

    loadview

---
+++ php-mvc/loadView.php
@@ -0,0 +1,12 @@
+<?php
+function loadView($name, $data = [])
+{
+  $viewPath = basePath("App/views/{$name}.view.php");
+
+  if (file_exists($viewPath)) {
+    extract($data);
+    require $viewPath;
+  } else {
+    echo "View '{$name} not found!'";
+  }
+}
--- php-mvc/superSimpleAutoload.php
+++ php-mvc/superSimpleAutoload.php
@@ -1,9 +1,5 @@
 <?php 
 //if you dont use composer (you should) you can autoload like this
-function basePath($path = '')
-{
-  return __DIR__ . '/' . $path;
-}
 
 spl_autoload_register(function($class){
     if(str_ends_with($class, "Controller"))
