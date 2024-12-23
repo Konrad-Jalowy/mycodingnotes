9b6deac73c9435d194bf788b129c309fbed83776
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-23 13:33:28 +0100

    useprev hook

---
+++ react/react-custom-hooks/usePrev.js
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+
+export function usePrev(reactiveVariable){
+
+    const currentValue = useRef(reactiveVariable);
+    const previousValue = useRef();
+
+    if(currentValue.current !== reactiveVariable){
+        previousValue.current = currentValue.current;
+        currentValue.current = reactiveVariable;
+    }
+    
+    return previousValue.current;
+};
