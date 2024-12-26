65ccefd90316b8d1c1d498868342c4e2a06dd8c7
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-26 10:31:44 +0100

    funcs 101 ts

---
+++ typescript-basic/funcs101.ts
@@ -0,0 +1,11 @@
+const add = (a: number, b: number): number => {
+    return a + b
+}
+
+const logMsg = (message: any): void => {
+    console.log(message)
+}
+
+let subtract = function (c: number, d: number): number {
+    return c - d
+}
