0dd45a612899b35ef9ce02c3464e44710ad75bcc
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-27 10:18:26 +0100

    while

---
+++ c#-basics/loops/while.cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int n = 0;
+            while (n < 5)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+    }
+}
