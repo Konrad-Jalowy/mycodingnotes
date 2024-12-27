237d7da376abdfb514619c3cc3b9fbf09760266f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-27 16:53:32 +0100

    streamwriter

---
+++ c#-basics/filesfolders/streamwriter1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string target = @"C:\csharpoutput";
+
+            Directory.SetCurrentDirectory(target);
+
+            string cwd = Directory.GetCurrentDirectory();
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(cwd, "WriteLines.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+        }
+    
+    }
+}
