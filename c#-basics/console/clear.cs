6a21aeebc9ba58c0eb081f3f9607299736cd1ed0
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:20:28 +0100

    clearing console

---
+++ c#-basics/console/clear.cs
@@ -0,0 +1,17 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Lorem ipsum");
+            Console.WriteLine("Hit any key to clear");
+            Console.Read();
+            Console.Clear();
+            Console.WriteLine("Hit any key to exit");
+            Console.Read();
+        }
+    }
+}
