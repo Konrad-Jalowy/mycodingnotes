dabcc68e0bd0f4db1de10e2fabe8e38aace47610
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 13:30:50 +0100

    counter1

---
+++ react/basics/Counter1.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+function Counter1(){
+    const [count, setCount] = useState(0)
+    return (
+        <>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(count + 1)}>Increment + 1</button>
+        </>
+    );
+};
+
+export {Counter1}
