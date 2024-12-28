daa6c46aaafbf98ab852fdf831479b1f5cec751a
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 17:45:55 +0100

    username

---
+++ c#-basics/varia/UserName.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current User: {Environment.UserName}");          
+        }
+    }
+}
