e433c7b81af22c3820fa8e6ddc38e5d3d9ef9f9e
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:11:34 +0100

    Readline

---
+++ c#-basics/console/readline1.cs
@@ -0,0 +1,12 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            Console.WriteLine(userInput);
+        }
+    }
+}
