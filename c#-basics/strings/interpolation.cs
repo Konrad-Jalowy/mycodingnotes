2eddba77b1f2ba51607c62fa9d9778c268519610
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:46:09 +0100

    interpolation

---
+++ c#-basics/strings/interpolation.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "John";
+            string lastName = "Doe";
+            Console.WriteLine($"Fullname: {firstName} {lastName}");
+        }
+    }
+}
