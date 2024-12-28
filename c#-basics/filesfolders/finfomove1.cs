3d3a4c0b892505be42a8cfce5d544b8bbfaf4853
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 11:29:20 +0100

    finfo move

---
+++ c#-basics/filesfolders/finfomove1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting App!");
+
+            string target = @"C:\csharpoutput";
+
+            Directory.SetCurrentDirectory(target);
+
+            string cwd = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(cwd, "WriteLines3.txt");
+
+            FileInfo myfile = new FileInfo(filePath);
+            if (myfile.Exists)
+            {
+                string destPath = Path.Combine(myfile.DirectoryName, "moved", $"Moved_{myfile.Name}");
+                myfile.MoveTo(destPath); //void
+            } 
+        }
+    }
+}
