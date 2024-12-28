9759b2b8999f29c07fabab160778aa5f3b517421
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:07:39 +0100

    Readmemd

---
+++ c#-basics/console/normalway.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting App!");
+            Console.WriteLine("Hi!");
+            Console.WriteLine("Exiting App!");
+        }
+    }
+}
