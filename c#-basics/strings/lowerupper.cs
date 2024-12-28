493835fd01e3831cae8df34acdabc87dfcdf7831
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:48:41 +0100

    lower upper

---
+++ c#-basics/strings/lowerupper.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "john";
+            string lastName = "DOE";
+            Console.WriteLine(firstName.ToUpper());
+            Console.WriteLine(lastName.ToLower());
+        }
+    }
+}
