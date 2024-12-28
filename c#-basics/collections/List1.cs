eaf2df422b883bd5a986fdf27a424eadba98e9f5
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 18:43:31 +0100

    list 1

---
+++ c#-basics/collections/List1.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            Console.WriteLine(numbers.Count); //5
+        }
+    }
+}
