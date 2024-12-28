45601c1cd1539f92581e545e0ef4e57bfa043c78
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:57:09 +0100

    trim char

---
+++ c#-basics/strings/trimwithchar.cs
@@ -0,0 +1,15 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char myChar = '0';
+            Console.WriteLine(myChar);
+            string time = "09";
+            Console.WriteLine(time.TrimStart('0'));
+            
+        }
+    }
+}
