14e906db1a924b27d5750d1c85257a70b891db64
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-28 12:06:00 +0100

    newline

---
+++ c#-basics/console/newline.cs
@@ -0,0 +1,13 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Starting App!" + Environment.NewLine);
+            Console.Write("Hi!" + Environment.NewLine);
+            Console.Write("Exiting App!" + Environment.NewLine);
+        }
+    }
+}
