e93320c94f66fb28fe3d2be1d2b5f0c191332364
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-29 13:42:00 +0100

    to string override

---
+++ c#-basics/classes/tostring.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Person p1 = new Person("John", "Doe");        
+            Console.WriteLine(p1.ToString()); //John Doe
+        }
+    }
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public Person(string first, string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+    }
+}
