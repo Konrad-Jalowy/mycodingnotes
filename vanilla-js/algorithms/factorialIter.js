2cde0d29c3a923fe45d0f5c440492c40a95a41b3
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-29 15:41:48 +0100

    factorial iterative

---
+++ vanilla-js/algorithms/factorialIter.js
@@ -0,0 +1,18 @@
+function factorialIter(num){
+
+    let result = num;
+
+    if(num < 0)
+        return -1;
+    if(num === 1 || num === 0)
+        return num;
+
+    while(num > 1){
+
+        num--;
+        result = result * num;
+    }
+
+    return result;
+
+}
