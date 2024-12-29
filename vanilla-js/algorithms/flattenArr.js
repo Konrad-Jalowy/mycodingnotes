b74b9d0ef8df611a959fc256e4ad9cc31cd3bccd
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-29 16:07:50 +0100

    flatten arr

---
+++ vanilla-js/algorithms/flattenArr.js
@@ -0,0 +1,26 @@
+const flatten = (nestedArr) => {
+    
+    const flatArr = [];
+
+    const _flatten = (arr) => {
+
+      let counter = 0
+
+      while (counter < arr.length) {
+
+        const val = arr[counter];
+
+        if (Array.isArray(val)) 
+          _flatten(val);
+        else 
+          flatArr.push(val);
+        
+        counter++;
+      }
+    
+    }
+
+    _flatten(nestedArr);
+
+    return flatArr;
+  }
