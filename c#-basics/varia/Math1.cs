bf504b997bbbe46aadcc101903c1cb3d90ea4d47
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-29 10:59:49 +0100

    math1

---
+++ c#-basics/varia/Math1.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Min(1, 2)); //1
+            Console.WriteLine(Math.Max(1, 2)); //2
+            Console.WriteLine(Math.Pow(10, 2)); //100
+            Console.WriteLine(Math.Abs(-10)); //10
+        }        
+    }
+}
