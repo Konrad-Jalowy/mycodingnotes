41ac7cbf2e156b75d4b009739d8036314662f0df
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-29 13:14:07 +0100

    my reduce right

---
+++ vanilla-js/polyfills/myReduceRight.js
@@ -0,0 +1,11 @@
+Array.prototype.myReduceRight= function(callbackFn, initialValue) {
+    var accumulator = initialValue;
+  for (var i = this.length - 1; i >= 0; i--) {
+      if (accumulator !== undefined) {
+        accumulator = callbackFn.call(undefined, accumulator, this[i],   i, this);
+      } else {
+        accumulator = this[i];
+      }
+    }
+    return accumulator;
+  }
