54aae437c5e7636968022376782e1f44751ce402
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-30 10:05:13 +0100

    factorial rec

---
+++ c#-basics/algorithms/factorialRec.cs
@@ -0,0 +1,34 @@
+using System;
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFactorial(-2)); //-1
+            Console.WriteLine(GetFactorial(-1)); //-1
+            Console.WriteLine(GetFactorial(0)); //1
+            Console.WriteLine(GetFactorial(1)); //1
+            Console.WriteLine(GetFactorial(2)); //2
+            Console.WriteLine(GetFactorial(3)); //6
+            Console.WriteLine(GetFactorial(4)); //24
+
+        }
+        private static long GetFactorial(int number)
+
+        {
+            if(number < 0)
+            {
+                return -1;
+            }
+
+            if (number == 1 || number == 0)
+            {
+                return 1;
+            }
+
+            return number * GetFactorial(number - 1);
+
+        }
+    }    
+}
