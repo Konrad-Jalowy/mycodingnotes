2a905e07ac36daea9a0a43e91c673ecedc24aa55
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-30 12:57:59 +0100

    json serialize

---
+++ c#-basics/102/jsonser1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.Json;
+namespace ConsoleApp13
+{
+    public class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string? Summary { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+            Console.WriteLine(jsonString);
+        }
+    }
+}
