3bb9ca560c764bff2f49a49236355fa33dd2cc63
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-30 16:06:33 +0100

    app js

---
+++ react/socketio/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import io from "socket.io-client";
+import {  useRef } from "react";
+import { MessagesController } from './components/MessagesController';
+
+
+function App() {
+
+  
+  const socket = useRef(null);
+
+  if(socket.current === null){
+    socket.current = io.connect("http://localhost:3001");
+  }
+
+  return (
+    <div className="App">
+      <MessagesController socket={socket.current}/>
+    </div>
+  );
+}
+
+export default App;
