8b99fc507aa9bc7c86acaf44e165f8c063eb109b
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-30 11:12:12 +0100

    list of class objects

---
+++ c#-basics/collections/List16.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+namespace ConsoleApp12
+{
+    class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string Job { get; set; }
+        public string City { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() { EmpID = 101, EmpName = "John Doe", Job = "Developer", City = "New York" },
+                new Employee() { EmpID = 102, EmpName = "Jane Doe", Job = "Manager", City = "New York" },
+                new Employee() { EmpID = 103, EmpName = "Jim Doe", Job = "Manager", City = "London" },
+                new Employee() { EmpID = 104, EmpName = "Janet Doe", Job = "Developer", City = "Warsaw" },
+                new Employee() { EmpID = 105, EmpName = "Jane Json", Job = "Manager", City = "Warsaw" },
+                new Employee() { EmpID = 106, EmpName = "Jim Xml", Job = "Developer", City = "London" },
+            };
+
+            Console.WriteLine(employees.Count); //6
+
+        }
+        
+    }    
+}
