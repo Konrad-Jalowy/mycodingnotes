84f3c9039bd09111a3a81ec6dacc7918a1abd1cc
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-01 10:55:49 +0100

    threading2

---
+++ c#-basics/102/threading2.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+        }
+    }
+}
