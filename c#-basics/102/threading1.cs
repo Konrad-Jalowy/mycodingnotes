3f041740422968cbfcfa1de23fe4a6010aca005b
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-01 10:53:12 +0100

    thread1

---
+++ c#-basics/102/threading1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading;
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(2000);
+            Console.WriteLine("Hello World after 2 seconds!");
+        }
+    }
+}
