26a980e57a6cfc4e031c8a2b77767eac6377a9cb
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-03 09:42:36 +0100

    spread with iterators

---
+++ vanilla-js/102/iterspread1.js
@@ -0,0 +1,17 @@
+let it = {
+    [Symbol.iterator]() {
+        var numbers = [1, 2, 3, 4, 5],
+            index = 0;
+
+        return {
+            next: function() {
+                return {
+                    done: (index === numbers.length) ? true : false,
+                    value: numbers[index++]
+                };
+            }
+        };
+    }
+};
+
+console.log( Math.max(...it) ); //5
