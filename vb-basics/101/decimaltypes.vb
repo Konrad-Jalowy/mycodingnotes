fb02ce61abb0baf0d56e5e2e70270e64b652d1b1
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-05 12:19:10 +0100

    dec

---
+++ vb-basics/101/decimaltypes.vb
@@ -0,0 +1,8 @@
+Module Program
+    Sub Main(args As String())
+        Dim num As Integer = 2.5 '2
+        Dim num2 As Single = 2.5 'single = float
+        Dim num3 As Double = 2.5 'double
+        Dim num4 As Decimal = 2.5 'better double
+    End Sub
+End Module
