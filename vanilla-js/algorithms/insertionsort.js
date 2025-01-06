8f14d19ac51cbc81ed23853e521323d27bc35d1b
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-06 14:05:15 +0100

    insertion sort

---
+++ vanilla-js/algorithms/insertionsort.js
@@ -0,0 +1,16 @@
+const insertionSort = (array) => {
+    for (let i = 1; i < array.length; i++) {
+      let currentElement = array[i];
+      let lastIndex = i - 1;
+  
+      while (lastIndex >= 0 && array[lastIndex] > currentElement) {
+        array[lastIndex + 1] = array[lastIndex];
+        lastIndex--;
+      }
+      array[lastIndex + 1] = currentElement;
+    }
+  
+    return array;
+  };
+  
+  console.log(insertionSort([2,1,9,76,4]))
