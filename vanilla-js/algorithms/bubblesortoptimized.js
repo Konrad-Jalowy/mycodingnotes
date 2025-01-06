53bd26510c3e870227918c95faf66636a79b2f7b
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-06 12:56:10 +0100

    optimized

---
+++ vanilla-js/algorithms/bubblesortoptimized.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+    let noswaps;
+    for (let i = arr.length; i > 0; i--) {
+        noswaps = true;
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+          noswaps = false;
+        }
+        if(noswaps) break;
+      }
+    }
+    return arr;
+  }
+  
