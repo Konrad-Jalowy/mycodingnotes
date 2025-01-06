f96a93073e465cc1533fbf336db829e7dc3753f9
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-06 16:38:53 +0100

    bubble sort optimized

---
+++ python/algorithms/bubblesortoptimized.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    for i in range(len(arr)-1, 0, -1):
+        noswaps = True
+        for j in range(0, i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                noswaps = False
+        if noswaps:
+            break
+    return arr
+
+print(bubble_sort([2,1,3,4, 5]))
