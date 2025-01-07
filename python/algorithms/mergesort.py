ee02780ee72e5617f8948cd9737acc8eb449522f
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-07 13:51:49 +0100

    mergesort in python

---
+++ python/algorithms/mergesort.py
@@ -0,0 +1,28 @@
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    return merge(merge_sort(left_half), merge_sort(right_half))
+
+
+def merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+    result += left[left_index:]
+    result += right[right_index:]
+    return result
+
+print(merge_sort([2,1,9,76,4]))
+print(merge_sort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]))
