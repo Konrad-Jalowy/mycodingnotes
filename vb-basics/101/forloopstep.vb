17c6ea00b7c6435922192ec302942a5a1ad4c42c
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-09 20:49:33 +0100

    forloop

---
+++ vb-basics/101/forloopstep.vb
@@ -0,0 +1,7 @@
+Module Program
+    Sub Main(args As String())
+        For i As Integer = 1 To 10 Step 2
+            Console.WriteLine($"Current value: {i}")
+        Next
+    End Sub
+End Module
