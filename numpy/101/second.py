e4f669bf0ef64c4ee51dee145dbf363195b87a02
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-15 10:17:14 +0100

    numpy

---
+++ numpy/101/second.py
@@ -0,0 +1,15 @@
+# Tablica zer
+zeros = np.zeros((3, 4))  # 3 wiersze, 4 kolumny
+print("Tablica zer:\n", zeros)
+
+# Tablica jedynek
+ones = np.ones((2, 3))
+print("Tablica jedynek:\n", ones)
+
+# Tablica liczb liniowo rozmieszczonych
+linspace = np.linspace(0, 10, 5)  # 5 liczb od 0 do 10
+print("Linspace:", linspace)
+
+# Tablica liczb całkowitych o równym kroku
+arange = np.arange(0, 10, 2)  # Od 0 do 10, co 2
+print("Arange:", arange)
