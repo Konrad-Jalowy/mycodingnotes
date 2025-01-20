3978000a1605540a358de51f1454bc09f8eb3c10
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-20 12:28:51 +0100

    buttonhelloworld2

---
+++ java-basics/swing/ButtonHelloWorld2.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonHelloWorld2 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Hello World Button App");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 200);
+
+            JButton button = new JButton("Click Me!");
+            JLabel label = new JLabel("", SwingConstants.CENTER);
+
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (label.getText().isEmpty()) {
+                        label.setText("Hello World");
+                    }
+                }
+            });
+
+            frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+            frame.add(button);
+            frame.add(label);
+
+            frame.setVisible(true);
+        });
+    }
+}
