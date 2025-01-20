fdef6b149c781377e57a24dcfad90df6983b45c7
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-20 10:36:13 +0100

    beeptest

---
+++ java-basics/102/BeepTest.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class BeepTest {
+    public static void main(String[] args) {
+        System.out.println("Testing beep...");
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
