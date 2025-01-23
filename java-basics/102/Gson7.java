57c841d54bc868ca78f4749eb626acd35ea5ee94
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-23 10:07:22 +0100

    gson7

---
+++ java-basics/102/Gson7.java
@@ -0,0 +1,34 @@
+import com.google.gson.Gson;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+
+class Post {
+    int id;
+    String title;
+    String body;
+}
+
+public class Gson7 {
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        // Użycie try-with-resources
+        Request request = new Request.Builder().url(url).build();
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                Gson gson = new Gson();
+                Post post = gson.fromJson(response.body().string(), Post.class);
+                System.out.println("Title: " + post.title);
+                System.out.println("Body: " + post.body);
+            } else {
+                System.out.println("Request failed: " + response.code());
+            }
+        } catch (IOException e) {
+            System.err.println("Błąd podczas wykonywania żądania: " + e.getMessage());
+        }
+    }
+}
+
