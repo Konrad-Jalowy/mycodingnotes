19541b4deec139113af21921e9642129ad9e1765
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-25 11:41:33 +0100

    fileoutput

---
+++ java-basics/102/FileOutputExample2.java
@@ -0,0 +1,14 @@
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class FileOutputExample2 {
+    public static void main(String[] args) {
+        String data = "Przykładowy tekst do zapisania!";
+        try (OutputStream outputStream = new FileOutputStream("output.txt")) {
+            outputStream.write(data.getBytes()); // Konwersja String do tablicy bajtów
+            System.out.println("Dane zapisane do pliku.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
