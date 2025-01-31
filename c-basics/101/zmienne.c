abd86a4f56aa3ba5304d279bd458e9851a671f18
Konrad-Jalowy <sitesystem@wp.pl>
2025-01-31 14:44:49 +0100

    zmienne w c

---
+++ c-basics/101/zmienne.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int liczba = 10;       // Liczba całkowita
+    float przecinek = 3.14; // Liczba zmiennoprzecinkowa
+    char znak = 'A';       // Jeden znak
+    printf("Liczba: %d, Float: %.2f, Znak: %c\n", liczba, przecinek, znak);
+    return 0;
+}
