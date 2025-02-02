bf0c64fef8c6e7562c288a67025e9ed6c8994bfb
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-02 15:52:19 +0100

    app py

---
+++ python-again/simplesqliteapp9/app.py
@@ -0,0 +1,34 @@
+import user_service
+
+def menu():
+    while True:
+        print("\n===== MENU =====")
+        print("1. Dodaj użytkownika")
+        print("2. Wyświetl użytkowników")
+        print("3. Edytuj użytkownika")
+        print("4. Usuń użytkownika")
+        print("5. Wyjście")
+        choice = input("Wybierz opcję: ")
+
+        if choice == "1":
+            first_name = input("Imię: ")
+            last_name = input("Nazwisko: ")
+            email = input("Email: ")
+            age = input("Wiek: ")
+            user_service.add_user(first_name, last_name, email, age)
+        elif choice == "2":
+            user_service.list_users()
+        elif choice == "3":
+            user_id = input("Podaj ID użytkownika do edycji: ")
+            user_service.update_user(user_id)
+        elif choice == "4":
+            user_id = input("Podaj ID użytkownika: ")
+            user_service.delete_user(user_id)
+        elif choice == "5":
+            print("👋 Zamykanie aplikacji...")
+            break
+        else:
+            print("⚠ Niepoprawny wybór!")
+
+if __name__ == "__main__":
+    menu()
