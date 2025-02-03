4100b74a2a6bd52f7d3f6c421c36f90be82c9a28
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-03 16:09:21 +0100

    app py

---
+++ python-again/flaskjwtrefresh/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from database.db_connector import init_db, db
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = "super_secret_key"  # Klucz JWT
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 900  # 15 minut (900 sekund)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 604800  # 7 dni (604800 sekund)
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies"]  # Akceptujemy JWT w nagłówkach i cookies
+app.config["JWT_COOKIE_SECURE"] = False  # Ustaw na True w produkcji (https)
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False  # ❌ Wyłączenie CSRF dla tokenów JWT
+
+
+init_db(app)
+jwt = JWTManager(app)
+
+from routes.auth_routes import auth_bp
+from routes.protected_routes import protected_bp
+
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(protected_bp, url_prefix="/api")
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
