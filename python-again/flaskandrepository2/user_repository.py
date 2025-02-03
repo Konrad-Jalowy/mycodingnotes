84867370060f75fcd88147d0a3f44bedf3bad40b
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-03 12:32:15 +0100

    user repository

---
+++ python-again/flaskandrepository2/user_repository.py
@@ -0,0 +1,29 @@
+from models.user import User
+from database.db_connector import db
+
+class UserRepository:
+    """Repozytorium dla operacji na użytkownikach"""
+
+    @staticmethod
+    def add_user(username, email):
+        user = User(username=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    @staticmethod
+    def get_user_by_id(user_id):
+        return User.query.get(user_id)
+
+    @staticmethod
+    def get_all_users():
+        return User.query.all()
+
+    @staticmethod
+    def delete_user(user_id):
+        user = User.query.get(user_id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return True
+        return False
