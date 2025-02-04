129ff7df5e331775e6462d25af90a1c51021caed
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-04 13:53:40 +0100

    models

---
+++ python-again/groupsproject2/app/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from app import db
+
+# Tabela pośrednia z datą dołączenia
+user_group = db.Table('user_group',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('group_id', db.Integer, db.ForeignKey('groups.id'), primary_key=True),
+    db.Column('joined_at', db.DateTime, default=datetime.utcnow)  # Nowe pole z datą dołączenia
+)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    groups = db.relationship('Group', secondary=user_group, backref=db.backref('members', lazy=True))
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
