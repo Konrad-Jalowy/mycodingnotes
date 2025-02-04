ff1d2d463e2b3b35acfa4a555bddafac34f3edd2
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-04 14:51:26 +0100

    models

---
+++ python-again/flaskrestfulgroups/app/models.py
@@ -0,0 +1,18 @@
+from app import db
+
+# Relacja Many-to-Many (Tabela pośrednia)
+user_group = db.Table('user_group',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('group_id', db.Integer, db.ForeignKey('groups.id'), primary_key=True)
+)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    groups = db.relationship('Group', secondary=user_group, backref='members')
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
