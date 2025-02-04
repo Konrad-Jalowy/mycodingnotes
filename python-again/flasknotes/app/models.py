e5835df9fbac1dc14e5389940564abf176f9b819
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-04 11:32:05 +0100

    models

---
+++ python-again/flasknotes/app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    notes = db.relationship('Note', backref='user', lazy=True)  # Relacja 1:N
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Klucz obcy
