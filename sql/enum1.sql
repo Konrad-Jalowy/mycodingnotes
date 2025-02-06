92b5adf5186adc431d2ece863ae9ed585b69e75f
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-06 11:10:03 +0100

    enum

--- sql/enum1.sql
+++ sql/enum1.sql
@@ -3,3 +3,11 @@ CREATE TABLE orders (
     user_id INT NOT NULL,
     status ENUM('pending', 'completed', 'cancelled') DEFAULT 'pending'
 );
+
+INSERT INTO orders (user_id, status) VALUES 
+(1, 'pending'),
+(2, 'completed'),
+(3, 'cancelled'),
+(4, DEFAULT); -- Tu zostanie ustawiony 'pending'
+
+SELECT * FROM orders;
