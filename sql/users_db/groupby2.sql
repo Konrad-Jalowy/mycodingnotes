7daaf9dcc9f3fc81e5a9288197a8b240880c8ddc
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-06 12:46:54 +0100

    groupby2

---
+++ sql/users_db/groupby2.sql
@@ -0,0 +1,4 @@
+SELECT users.first_name, users.last_name, COALESCE(SUM(orders.total_price), 0) AS total_spent
+FROM users
+LEFT JOIN orders ON users.id = orders.user_id
+GROUP BY users.id, users.first_name, users.last_name;
