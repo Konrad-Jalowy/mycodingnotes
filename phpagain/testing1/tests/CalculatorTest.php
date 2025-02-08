0f01634c234a7caafe83b5ef5b816070586409b0
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-08 09:28:15 +0100

    expect exception

--- phpagain/testing1/tests/CalculatorTest.php
+++ phpagain/testing1/tests/CalculatorTest.php
@@ -9,4 +9,10 @@ class CalculatorTest extends TestCase {
         $this->assertEquals(5, $calc->add(2, 3));
         $this->assertEquals(0, $calc->add(-1, 1));
     }
+
+    public function testDivideByZeroThrowsException() {
+        $calc = new Calculator();
+        $this->expectException(\InvalidArgumentException::class);
+        $calc->divide(10, 0);
+    }
 }
