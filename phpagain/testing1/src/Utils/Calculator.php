c05308dcff1e9fbcfc22a3bd453965655bebba91
Konrad-Jalowy <sitesystem@wp.pl>
2025-02-08 09:27:32 +0100

    another method

--- phpagain/testing1/src/Utils/Calculator.php
+++ phpagain/testing1/src/Utils/Calculator.php
@@ -6,4 +6,10 @@ class Calculator {
     public function add($a, $b) {
         return $a + $b;
     }
+    public function divide($a, $b) {
+        if ($b === 0) {
+            throw new \InvalidArgumentException("Cannot divide by zero");
+        }
+        return $a / $b;
+    }
 }
